a15d9ac86b1a1fc33e21888d87f19d88923d9a8f
Chris Lopez <lopez.chris98@gmail.com>
2021-07-28 14:33:07 -0400

    Added functionality to save CV button that removes input fields and leaves the user inputted

--- src/components/Buttons/SavePageBtn.js
+++ src/components/Buttons/SavePageBtn.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 function SavePageBtn(){
+
+    const formatResume = () => {
+        // disable buttons
+        const buttons = document.getElementsByTagName('button');
+        Array.from(buttons, (button) => (button.style.display = 'none'));
+
+        // disable inputs and remove display of input boxes
+        const inputs = document.getElementsByTagName('input');
+        Array.from(inputs, (inputs) => {
+            inputs.style.border = 'none';
+            inputs.style.backgroundColor = 'white';
+            
+        });
+
+
+    };
+
+
+
+
     return (
-        <button className="save-page-btn">Save Resume</button>
+        <button className="save-page-btn" onClick={formatResume}>Save Resume</button>
     );
 
 
